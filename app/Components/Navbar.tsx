9062bc23bff9d4d8fbfb069ac853a213a95be9f8
theahmetgg <ahmet1998gulmez.com>
2024-05-17 16:51:22 +0300

    some components have been created

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,8 +1,25 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const router = useRouter();
+  return (
+    <div className="w-full py-4 flex items-center justify-between">
+      <div className="left"> </div>
+      <div className="search-container flex shrink-0 w-full gap-2 sm:w-fit">
+        <Button
+          className="source-code flex items-center gap-2"
+          onClick={() => {
+            router.push("https//github.com/theahmetgg/weather-app");
+          }}
+        >
+          Source Code
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}><ThemeProvider>
-        </ThemeProvider></body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
