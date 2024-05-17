f31838f094dccea5be033869e6827c0e4fda4b95
theahmetgg <ahmet1998gulmez.com>
2024-05-17 19:29:47 +0300

    context created

---
+++ app/context/globalContext.js
@@ -0,0 +1,7 @@
+"use Client";
+
+import React from "react";
+import { useContext, createContext } from "react";
+
+const GlobalContext = createContext;
+const GlobalContextUpdate = createContext;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
-import Image from "next/image";
 import Navbar from "./Components/Navbar";
-
+export const metadata = {
+  title: "Weather App",
+  description: "So Cool",
+};
 export default function Home() {
   return (
     <main className="mx-[1rem] lg:mx=[2rem] xl:mx-[6rem] 2xl:mx-[16rem] m-auto">
